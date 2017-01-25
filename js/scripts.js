ef2a6668f02d7ac90d2ef268fdaf4a295773c4b6
Caitlin Hines and Keely Glenn <caitlin@>
2017-01-25 11:38:41 -0800

    add basic premise for contructor and prototype

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,9 @@
 // backend
+function Player(roll, rollArray, rollSum){
+  this.roll = roll;
+  this.rollArray = rollArray;
+  this.rollSum = rollSum;
+}
 
   function randomRoll() {
       return Math.floor(Math.random() * (1 + 6 - 1) ) + 1;
@@ -6,22 +11,29 @@
 
   var diceNumberArrays= [];
 
-
+ Player.prototype.oneRule = function() {
+   if (this.roll === 1) {
+     this.rollArray = 0;
+   }
+ }
 
 // frontend
 $(document).ready(function() {
   $("#playerOneRoll").click(function(event){
     event.preventDefault();
 
-// roll random numbers
+// roll random numbers then lists each roll in an array
     var roll= randomRoll();
     diceNumberArrays.push(roll);
 
-// add tallies
+// add tallies for total tally count
     var tallyTotal = 0;
     for (var i =0; i < diceNumberArrays.length; i++) {
       tallyTotal += parseInt(diceNumberArrays[i]);
     };
+
+    var playerOne = new Player(roll, diceNumberArrays, tallyTotal);
+
 
     $("#playerOneCurrentDice").text(roll);
     $("#playerOneTally").text(diceNumberArrays);
